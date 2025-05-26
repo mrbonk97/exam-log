18648645a6facaaab827f382113045d14796972a
mrbonk97 <mrbonk97@gmail.com>
2025-05-26 15:26:24 +0900

    fix: 회원 삭제 쿼리문 수정

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,7 +3,7 @@ import { errorFactory } from "@/lib/el-error";
 import { verifyJwt } from "@/lib/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-const SQL = "DELETE * FROM EL_USER WHERE USER_ID = :user_id";
+const SQL = "DELETE FROM EL_USER WHERE ID = :id";
 
 export const DELETE = async (req: NextRequest) => {
   let conn = null;
@@ -15,9 +15,27 @@ export const DELETE = async (req: NextRequest) => {
 
     conn = await getConn();
     const res = await conn.execute(SQL, [userId], { autoCommit: true });
-    if (res.rowsAffected == 1)
-      return NextResponse.json({ code: "success", message: `회원을 삭제했습니다.ID: ${userId}` });
-    else throw errorFactory("ERROR_00");
+
+    if (res.rowsAffected != 1) throw errorFactory("ERROR_00");
+
+    const response = NextResponse.json({
+      code: "success",
+      message: `회원을 삭제했습니다.ID: ${userId}`,
+    });
+
+    response.cookies.set("access_token", "deleted", {
+      maxAge: 0,
+      httpOnly: true,
+      sameSite: "lax",
+    });
+
+    response.cookies.set("refresh_token", "deleted", {
+      maxAge: 0,
+      httpOnly: true,
+      sameSite: "lax",
+    });
+
+    return res;
   } catch (err) {
     console.log(err);
     return NextResponse.json({ code: "error", message: "서버에서 오류가 발생했습니다." });
